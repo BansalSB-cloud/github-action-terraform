fd527d3803d4773eabee59ead4c693ae5c47fdcd
BansalSB-cloud <bansal.sangeeta1105@gmail.com>
2026-09-13 10:53:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,52 +1,3 @@
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Name = "pragra-vpc"
-  }
-}
-
-# Public Subnet 1
-resource "aws_subnet" "public_1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ca-central-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-1"
-  }
-}
-
-# Security Group for EC2
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-sg"
-  description = "Security group for EC2"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ec2-sg"
-  }
-}
-
-# S3 Bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket        = "${var.bucket_name}-${random_id.rand.hex}"
   force_destroy = true
@@ -56,13 +7,15 @@ resource "random_id" "rand" {
   byte_length = 4
 }
 
-# EC2 Instance
+#Deploy an IAM Role with PowerUserIAMPolicy
+#Deploy an RDS Instance
+
 resource "aws_instance" "public_ec2" {
   ami                    = var.ec2_ami
   instance_type          = var.instance_type
   key_name               = var.ec2_key_pair
-  subnet_id              = aws_subnet.public_1.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  subnet_id              = data.aws_ssm_parameter.resources["subnet1_id"].value
+  vpc_security_group_ids = [data.aws_ssm_parameter.resources["ec2_sg_id"].value]
 
   tags = {
     Name = "Public_EC2"
