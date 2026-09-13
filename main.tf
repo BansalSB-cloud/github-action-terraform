f731a2f4fbfe0001e1a1dfdc9264e2ab178739fb
BansalSB-cloud <bansal.sangeeta1105@gmail.com>
2026-09-13 10:29:37 -0400

    Force workflow refresh

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ resource "aws_instance" "public_ec2" {
   ami                    = var.ec2_ami
   instance_type          = var.instance_type
   key_name               = var.ec2_key_pair
-  subnet_id              = aws_subnet.public_1.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  subnet_id              = data.aws_ssm_parameter.resources["subnet1_id"].value
+  vpc_security_group_ids = [data.aws_ssm_parameter.resources["ec2_sg_id"].value]
 
   tags = {
     Name = "Public_EC2"
   }
-}
+}# Updated
--- terraform.tf
+++ terraform.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket = "sangeeta-tfstate-2026"
-    key    = "env/dev/terraform.tfstate"
+    key    = "env/dev/terraform-resources.tfstate"
     region = "ca-central-1"
   }
 }
