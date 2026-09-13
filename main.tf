26068c0812a8b94c7a63a4b904c92cc7d6647e7c
BansalSB-cloud <bansal.sangeeta1105@gmail.com>
2026-09-12 22:30:28 -0400

    Fix Terraform configurations - add data source and provider

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+   data "aws_ssm_parameter" "resources" {
+     for_each = toset(["subnet1_id", "ec2_sg_id"])
+     name     = each.value
+   }
 resource "aws_s3_bucket" "my_bucket" {
   bucket        = "${var.bucket_name}-${random_id.rand.hex}"
   force_destroy = true
@@ -14,10 +18,9 @@ resource "aws_instance" "public_ec2" {
   ami                    = var.ec2_ami
   instance_type          = var.instance_type
   key_name               = var.ec2_key_pair
-  subnet_id              = data.aws_ssm_parameter.resources["subnet1_id"]
-  vpc_security_group_ids = [data.aws_ssm_parameter.resources["ec2_sg_id"]]
-
-  tags = {
-    Name = "Public_EC2"
-  }
-}
+  subnet_id              = data.aws_ssm_parameter.resources["subnet1_id"].value
+  vpc_security_group_ids = [data.aws_ssm_parameter.resources["ec2_sg_id"].value]
+     tags = {
+       Name = "Public_EC2"
+     }
+   }
